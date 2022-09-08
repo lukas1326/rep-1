60e232485a57800155f95d0e92a4a349463a20a2
olga <aprilium@live.com>
2022-09-08 20:42:01 +0300

    ex.3-1

--- my.py
+++ my.py
@@ -1,5 +1,17 @@
-print("ol"*5)
-# it is the note
-print ("hjkjhghj")
-#TODO: This is todo 
-#FIXME: This is a fixme
+# Write a program that asks for and saves a username
+# Ask a question about the user's age, using the username in the question.
+# Shows in how many years the user will be 100 years old smile
+# BONUS: 
+# then you will need two additional lines:
+# import datetime # let's talk about imports separately
+# currentYear = datetime.datetime.now (). yearLet the program also show the year when the user will be 100 years old.
+# It could use a variable with the current year.
+# It would be even better to get the current year automatically
+
+import datetime
+
+user_name = input("What is your name?  ")
+
+user_age = input(f"How old are you, {user_name}?  ")
+
+print(f"You will have 100 years in {100 - int(user_age) + datetime.datetime.now().year}.")
