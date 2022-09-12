2ee642c6b942841094b7243c9e80642672f92a8d
lukas1326 <olga.lukasevich@gmail.com>
2022-09-12 21:46:37 +0300

    done

--- day3_exercise1_assignment.py
+++ day3_exercise1_assignment.py
@@ -32,10 +32,10 @@ else:
 
 # Example2: 1.5 years of experience, 1500 Euro salary, no bonus(0)
 
-sal = float(input('Your salary is  :')) 
-period = float(input('How many years have you worked here? '))
-bonus_rate=0.15
-if period >=2:
-    print(f'{period} years of experience,{sal} Euro salary, the bonus will be {(sal)*bonus_rate *(period-2)}')
-else:
-    print(f'{period} years of experience,{sal} Euro salary, no bonus(0)')
+# sal = float(input('Your salary is  :')) 
+# period = float(input('How many years have you worked here? '))
+# bonus_rate=0.15
+# if period >=2:
+#     print(f'{period} years of experience,{sal} Euro salary, the bonus will be {(sal)*bonus_rate *(period-2)}')
+# else:
+#     print(f'{period} years of experience,{sal} Euro salary, no bonus(0)')
