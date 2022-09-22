75e78d1579db98436cf66be02ecd0a31798458d6
Olga <aprilium@live.com>
2022-09-22 20:57:45 +0300

    done

---
+++ func_7_2.py
@@ -0,0 +1,28 @@
+# 2. Palindrome
+
+
+
+# Write the function is_palindrome(text)
+
+# which returns a bool True or False depending on whether the word or sentence is read equally from both sides.
+
+# PS You can start with a one-word solution from the beginning, but the full solution will ignore whitespace and uppercase and lowercase letters.
+
+
+
+# is_palindrome ("Alus ari i ra    sula") -> True
+
+# is_palindrome("ABa") -> True
+
+# is_palindrome("nava") -> False
+
+def is_palindrome(some_text):
+    format_text = some_text.lower().replace(" ","")
+    if format_text==format_text[::-1]:
+        result=True
+    else:
+        result=False
+    return result
+
+my_text  = input("Input text: ")
+print(is_palindrome(my_text))
